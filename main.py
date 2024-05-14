cc63bbcc2f7ea3c7ba25ae742e798de00de17f2d
rafatoscanodev <99692598+rafatoscanodev@users.noreply.github.com>
2024-05-14 00:39:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 import streamlit as st
 import pandas as pd
-import requests
 
-# URL dos dados JSON
-url = "http://dados.recife.pe.gov.br/dataset/eb9b8a72-6e51-4da2-bc2b-9d83e1f198b9/resource/b4c77553-4d25-4e3a-adb2-b225813a02f1/download/metadados_empativas.json"
+# URL dos dados CSV
+url = "http://dados.recife.pe.gov.br/dataset/eb9b8a72-6e51-4da2-bc2b-9d83e1f198b9/resource/87fc9349-312c-4dcb-a311-1c97365bd9f5/download/empresasativender.csv"
 
 # Função para carregar dados
 @st.cache
 def load_data(url):
-    response = requests.get(url)
-    data = response.json()
-    return data
+    df = pd.read_csv(url)
+    return df
 
 # Carregar os dados
-data = load_data(url)
+df = load_data(url)
 
-# Converter para DataFrame
-df = pd.DataFrame(data)
+# Ordenar os dados pela coluna 'nome_bairro'
+df_sorted = df.sort_values(by='nome_bairro')
 
 # Exibir título e descrição do aplicativo
-st.title("Visualizações de Empatias em Recife")
-st.write("Dados carregados da URL fornecida.")
-
-# Exibir DataFrame
-st.write("Primeiros registros do DataFrame:")
-st.write(df.head())
+st.title("Empresas Ativas em Recife")
+st.write("Dados carregados da URL fornecida e ordenados por 'nome_bairro'.")
 
 # Exibir colunas do DataFrame
 st.write("Colunas do DataFrame:")
 st.write(df.columns)
 
-
-
-
+# Exibir DataFrame ordenado
+st.write("Primeiros registros do DataFrame ordenado:")
+st.write(df_sorted.head(20))  # Exibe as primeiras 20 linhas do DataFrame ordenado
